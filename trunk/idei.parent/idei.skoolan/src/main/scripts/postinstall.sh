d172ce92e45a8ea433227c09184e70204ac295ab
eagirrezabal <eagirrezabal@a5e02eba-b892-4d49-8416-2aa2ae7277cf>
2014-07-30 01:23:06 +0000

    re-commit

---
+++ trunk/idei.parent/idei.skoolan/src/main/scripts/postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TOMCAT_CONF_DIR=/etc/tomcat6
+RPM_IDEI_TMP_DIR=/tmp/idei.skoolan-%{release}
+
+echo IDEI SKOOLAN RPM TMP DIR:
+echo ${RPM_AIO_TMP_DIR}
+
+echo
+echo Post install executing .......
+echo
+
+
+
+
+
+
